50f1c2f90ba3aea9089b8a63acbc2b41f56fdf29
Berkay Sahin <berkaysahin@eskisehir.edu.tr>
2022-09-22 17:21:49 +0300

    Java
    This programme will find the prime number

--- JavaDemos/miniProjeAsalSayi/src/Main.java
+++ JavaDemos/miniProjeAsalSayi/src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        int number = 6;
+        int number = 1;
        /* int remainder = number % 2;
         System.out.println(remainder);*/
 
-        if(number == 2 ){
+        if(number ==2 ){
             System.out.println("The number is Prime");
+            return;
+        }
+        else if(number <2){
+            System.out.println("Invalid Value!");
         }
 
         for (int i = 2; i < number; i++) {
